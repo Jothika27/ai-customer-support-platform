f0e32adfa73e3b157f618406b7b64b039deb3bd7
Jothika27 <jothikajo272001@gmail.com>
2026-06-01 11:11:00 +0530

    Initial commit

---
+++ src/test/java/com/jo/customer_support/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo.customer_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
